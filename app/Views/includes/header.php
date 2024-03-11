84a28c768d2c2bccb8ffcbfe0a4c73b79c06f87f
DJ <djogiya562@gmail.com>
2024-03-11 16:15:35 -0700

    Make it short

---
+++ app/Views/includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?= $title. " | ". getenv('APP_NAME'); ?></title>
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/all.min.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/nice-select.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/owl.min.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/magnific-popup.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/flaticon.css');?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/main.css');?>">
+
+    <link rel="shortcut icon" href="<?= base_url('assets/images/icon.png');?>" type="image/x-icon">
+</head>
